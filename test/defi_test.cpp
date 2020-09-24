c3a15a891fc1dbd54d691a78bd8fe7ac60de1c5a
stone <stone.jobs@protonmail.com>
2020-09-24 21:40:32 +0800

    Add DeFi test

--- test/defi_test.cpp
+++ test/defi_test.cpp
@@ -523,17 +523,244 @@ BOOST_AUTO_TEST_CASE(reward)
         BOOST_CHECK(it != destIdx.end() && it->nReward == 1481480742);
     }
 
-    // test all reward
-    nReward = r.GetSectionReward(forkid2, uint256(2939, uint224(0)));
-    CDeFiRelationGraph relationReward;
-    {
-        CDeFiRewardSet reward = r.ComputePromotionReward(nReward / 2, balance, profile2.defi.mapPromotionTokenTimes, relationReward);
-        for (auto& x : reward)
-        {
-            cout << "promotion reward, destination: " << CAddress(x.dest).ToString() << ", reward: " << x.nReward << endl;
-        }
-    }
-
     // boost::filesystem::remove_all(logPath);
 }
+
+BOOST_AUTO_TEST_CASE(reward2)
+{
+    CDeFiForkReward r;
+    uint256 forkid;
+    RandGeneretor256(forkid.begin());
+
+    // test ExistFork and AddFork
+    BOOST_CHECK(!r.ExistFork(forkid));
+
+    CProfile profile;
+    profile.strName = "BBC Test2";
+    profile.strSymbol = "BBCB";
+    profile.nVersion = 1;
+    profile.nMinTxFee = NEW_MIN_TX_FEE;
+    profile.nMintReward = 0;
+    profile.nAmount = 10000000 * COIN; // 首期发行一千万母币
+    profile.nJointHeight = 15;
+    profile.nForkType = FORK_TYPE_DEFI;
+    profile.defi.nMintHeight = 20;
+    profile.defi.nMaxSupply = 1000000000 * COIN; // BTCA 总共发行十亿枚
+    profile.defi.nCoinbaseType = SPECIFIC_DEFI_COINBASE_TYPE;
+    profile.defi.mapCoinbasePercent = { { 259200, 10 }, { 777600, 8 }, { 1814400, 5 }, { 3369600, 3 }, { 5184000, 2 } }; // 发行阶段，半年，1年（用高度表示），参考BTCA白皮书，月增长原有基数的10%，8%
+    profile.defi.nRewardCycle = 5;                                                                                       // 60 * 24  per day once reward
+    profile.defi.nSupplyCycle = 150;                                                                                     // 60 * 24 * 30  per month once supply
+    profile.defi.nStakeMinToken = 100 * COIN;                                                                            // min token required, >= 100, can be required to join this defi game
+    profile.defi.nStakeRewardPercent = 50;                                                                               // 50% of supply amount per day
+    profile.defi.mapPromotionTokenTimes.insert(std::make_pair(10000, 10));                                               // 用于推广收益，小于等于10000的部分，要放大10倍
+    r.AddFork(forkid, profile);
+
+    BOOST_CHECK(r.ExistFork(forkid));
+    BOOST_CHECK(r.GetForkProfile(forkid).strSymbol == "BBCB");
+
+    int64 nReward = r.GetSectionReward(forkid, uint256(24, uint224(0)));
+
+    CAddress A("1632srrskscs1d809y3x5ttf50f0gabf86xjz2s6aetc9h9ewwhm58dj3");
+    CAddress a1("1f1nj5gjgrcz45g317s1y4tk18bbm89jdtzd41m9s0t14tp2ngkz4cg0x");
+    CAddress a11("1pmj5p47zhqepwa9vfezkecxkerckhrks31pan5fh24vs78s6cbkrqnxp");
+    CAddress a111("1bvaag2t23ybjmasvjyxnzetja0awe5d1pyw361ea3jmkfdqt5greqvfq");
+    CAddress a2("1ab1sjh07cz7xpb0xdkpwykfm2v91cvf2j1fza0gj07d2jktdnrwtyd57");
+    CAddress a21("1782a5jv06egd6pb2gjgp2p664tgej6n4gmj79e1hbvgfgy3t006wvwzt");
+    CAddress a22("1c7s095dcvzdsedrkpj6y5kjysv5sz3083xkahvyk7ry3tag4ddyydbv4");
+    CAddress a221("1ytysehvcj13ka9r1qhh9gken1evjdp9cn00cz0bhqjqgzwc6sdmse548");
+    CAddress a222("16aaahq32cncamxbmvedjy5jqccc2hcpk7rc0h2zqcmmrnm1g2213t2k3");
+    CAddress a3("1vz7k0748t840bg3wgem4mhf9pyvptf1z2zqht6bc2g9yn6cmke8h4dwe");
+    CAddress B("1fpt2z9nyh0a5999zrmabg6ppsbx78wypqapm29fsasx993z11crp6zm7");
+    CAddress b1("1rampdvtmzmxfzr3crbzyz265hbr9a8y4660zgpbw6r7qt9hdy535zed7");
+    CAddress b2("1w0k188jwq5aenm6sfj6fdx9f3d96k20k71612ddz81qrx6syr4bnp5m9");
+    CAddress b3("196wx05mee1zavws828vfcap72tebtskw094tp5sztymcy30y7n9varfa");
+    CAddress b4("19k8zjwdntjp8avk41c8aek0jxrs1fgyad5q9f1gd1q2fdd0hafmm549d");
+    CAddress C("1965p604xzdrffvg90ax9bk0q3xyqn5zz2vc9zpbe3wdswzazj7d144mm");
+
+    // test stake reward
+    // B = 1/1
+    map<CDestination, int64> balance;
+    balance = map<CDestination, int64>{
+        { A, 0 },
+        { B, 100 * COIN },
+    };
+    std::cout << "nReward " << nReward << std::endl;
+    {
+        CDeFiRewardSet reward = r.ComputeStakeReward(profile.defi.nStakeMinToken, (nReward), balance);
+        BOOST_CHECK(reward.size() == 1);
+        auto it = reward.begin();
+
+        // B的持币量排名 1
+        // 各个地址的排名相加 1
+        // nReward = 925925925
+        // 1 / 1 * nReward * 50%
+        BOOST_CHECK(it->dest == B && it->nReward == (nReward));
+    }
+
+    balance = map<CDestination, int64>{
+        { A, 0 },
+        { a1, 100 * COIN },   //  rank 1
+        { a11, 1000 * COIN }, // rank 5
+        { a111, 100 * COIN }, // rank 1
+        { a221, 105 * COIN }, // rank 4
+        { a222, 100 * COIN }, // rank 1
+    };
+    {
+        CDeFiRewardSet reward = r.ComputeStakeReward(profile.defi.nStakeMinToken, nReward, balance);
+        BOOST_CHECK(reward.size() == 5);
+        auto& destIdx = reward.get<0>();
+        auto it = destIdx.find(a1);
+        // 1 / 12 * nReward
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (2777777777));
+        std::cout << "a1 " << it->nReward << std::endl;
+        it = destIdx.find(a11);
+        // 5 / 12 * nReward
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (13888888888));
+        it = destIdx.find(a111);
+        // 1 / 12 * nReward
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (2777777777));
+        it = destIdx.find(a221);
+        // 4 / 12 * nReward
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (11111111111));
+
+        it = destIdx.find(a222);
+        // 1 / 12 * nReward
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (2777777777));
+    }
+
+    // test promotion reward
+    balance = map<CDestination, int64>{
+        { A, 10000 * COIN },
+        { a1, 100000 * COIN },
+        { a11, 100000 * COIN },
+        { a111, 100000 * COIN },
+        { a2, 1 * COIN },
+        { a21, 1 * COIN },
+        { a22, 12000 * COIN },
+        { a221, 18000 * COIN },
+        { a222, 5000 * COIN },
+        { a3, 1000000 * COIN },
+        { B, 10000 * COIN },
+        { b1, 10000 * COIN },
+        { b2, 11000 * COIN },
+        { b3, 5000 * COIN },
+        { b4, 50000 * COIN },
+        { C, 19568998 * COIN },
+    };
+
+    {
+        CDeFiRewardSet reward = r.ComputeStakeReward(profile.defi.nStakeMinToken, (nReward), balance);
+        BOOST_CHECK(reward.size() == 14);
+        auto& destIdx = reward.get<0>();
+
+        // 3 / 98 * nReward
+        auto it = destIdx.find(A);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (1020408163));
+
+        // 10 / 98 * nReward
+        it = destIdx.find(a1);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (3401360544));
+
+        // 10 / 98 * nReward
+        it = destIdx.find(a11);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (3401360544));
+
+        // 10 / 98 * nReward
+        it = destIdx.find(a111);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (3401360544));
+
+        // 7 / 98 * nReward
+        it = destIdx.find(a22);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (2380952380));
+
+        // 8 / 98 * nReward
+        it = destIdx.find(a221);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (2721088435));
+
+        // 1 / 98 * nReward
+        it = destIdx.find(a222);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (340136054));
+
+        // 13 / 98 * nReward
+        it = destIdx.find(a3);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (4421768707));
+
+        // 3 / 98 * nReward
+        it = destIdx.find(B);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (1020408163));
+
+        // 3 / 98 * nReward
+        it = destIdx.find(b1);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (1020408163));
+
+        // 6 / 98 * nReward
+        it = destIdx.find(b2);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (2040816326));
+
+        // 1 / 98 * nReward
+        it = destIdx.find(b3);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (340136054));
+
+        // 9 / 98 * nReward
+        it = destIdx.find(b4);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (3061224489));
+
+        // 14 / 98 * nReward
+        it = destIdx.find(C);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == (4761904761));
+    }
+
+    map<CDestination, CAddrInfo> mapAddress;
+    mapAddress = map<CDestination, CAddrInfo>{
+        { a1, CAddrInfo(A, A, uint256(1)) },
+        { a11, CAddrInfo(A, a1, uint256(1)) },
+        { a111, CAddrInfo(A, a11, uint256(1)) },
+        { a2, CAddrInfo(A, A, uint256(1)) },
+        { a21, CAddrInfo(A, a2, uint256(1)) },
+        { a22, CAddrInfo(A, a2, uint256(1)) },
+        { a221, CAddrInfo(A, a22, uint256(1)) },
+        { a222, CAddrInfo(A, a22, uint256(1)) },
+        { a3, CAddrInfo(A, A, uint256(1)) },
+        { b1, CAddrInfo(B, B, uint256(1)) },
+        { b2, CAddrInfo(B, B, uint256(1)) },
+        { b3, CAddrInfo(B, B, uint256(1)) },
+        { b4, CAddrInfo(B, B, uint256(1)) },
+    };
+
+    CDeFiRelationGraph relation;
+    for (auto& x : mapAddress)
+    {
+        BOOST_CHECK(relation.Insert(x.first, x.second.destParent, x.second.destParent));
+    }
+    {
+        CDeFiRewardSet reward = r.ComputePromotionReward(nReward, balance, profile.defi.mapPromotionTokenTimes, relation);
+        BOOST_CHECK(reward.size() == 6);
+        auto& destIdx = reward.get<0>();
+
+        // 515102 / 816312 * nReward
+        auto it = destIdx.find(A);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == 21033706066);
+
+        // 58 / 816312 * nReward
+        it = destIdx.find(a1);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == 2368375);
+
+        // 46 / 816312 * nReward
+        it = destIdx.find(a11);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == 1878366);
+
+        // 43 / 816312 * nReward
+        it = destIdx.find(a2);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == 1755864);
+
+        // 50026 / 816312 * nReward
+        it = destIdx.find(a22);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == 2042764694);
+
+        // 251037 / 816312 * nReward
+        it = destIdx.find(B);
+        BOOST_CHECK(it != destIdx.end() && it->nReward == 10250859965);
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
