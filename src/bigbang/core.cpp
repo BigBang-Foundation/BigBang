4679d32a1842c6b9ebc0e474217e4887eb50b4fe
StoneJobs <70944630+StoneJobs@users.noreply.github.com>
2020-11-02 17:24:58 +0800

    Update blacklist height

--- src/bigbang/core.cpp
+++ src/bigbang/core.cpp
@@ -133,7 +133,7 @@ static const map<uint256, map<int, set<CDestination>>> mapDeFiBlacklist = {
         uint256("0006d42cd48439988e906be71b9f377fcbb735b7905c1ec331d17402d75da805"),
         {
             {
-                510000,
+                500824,
                 {
                     bigbang::CAddress("103vf0z8f5kry0937ar3ac864cbhkfh8efmmy8mxxy27kaq5sf3svbare"),
                     bigbang::CAddress("1m8sm8bsydnwaabhhfzjgwnxd2rd879g2cnj1nw8d5j3bhv29ftp5z2bs"),
