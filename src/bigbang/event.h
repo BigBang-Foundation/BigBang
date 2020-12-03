017a49a407eeacb4e2b01ff754d1517f76684856
MathxH Chen <brainfvck@foxmail.com>
2020-12-03 10:50:24 +0800

    Add RPCMod Event listener

--- src/bigbang/event.h
+++ src/bigbang/event.h
@@ -25,7 +25,11 @@ enum
     EVENT_BLOCKMAKER_ENROLL,
     EVENT_BLOCKMAKER_DISTRIBUTE,
     EVENT_BLOCKMAKER_PUBLISH,
-    EVENT_BLOCKMAKER_AGREE
+    EVENT_BLOCKMAKER_AGREE,
+
+    ///////// RPCMod ////////////////
+    EVENT_RPCMOD_UPDATE_NEW_BLOCK,
+    EVENT_RPCMOD_UPDATE_NEW_TRANSACTION
 };
 
 class CBlockMakerEventListener;
@@ -41,6 +45,61 @@ public:
     virtual ~CBlockMakerEventListener() {}
     DECLARE_EVENTHANDLER(CEventBlockMakerUpdate);
     DECLARE_EVENTHANDLER(CEventBlockMakerAgree);
+};
+
+template <int type, typename L, typename D>
+class CRPCModEventData : public CEvent
+{
+    friend class CStream;
+
+public:
+    CRPCModEventData(uint64 nNonceIn, const uint256& hashForkIn, int64 nChangeIn)
+      : CEvent(nNonceIn, type), hashFork(hashForkIn), nChange(nChangeIn) {}
+    virtual ~CRPCModEventData() {}
+    virtual bool Handle(CEventListener& listener)
+    {
+        try
+        {
+            return (dynamic_cast<L&>(listener)).HandleEvent(*this);
+        }
+        catch (std::bad_cast&)
+        {
+            return listener.HandleEvent(*this);
+        }
+        catch (std::exception& e)
+        {
+            StdError(__PRETTY_FUNCTION__, e.what());
+        }
+        return false;
+    }
+
+protected:
+    template <typename O>
+    void BlockheadSerialize(CStream& s, O& opt)
+    {
+        s.Serialize(hashFork, opt);
+        s.Serialize(data, opt);
+    }
+
+public:
+    uint256 hashFork;
+    int64 nChange;
+    D data;
+};
+
+class CRPCModEventListener;
+#define TYPE_RPCMOD_EVENT(type, body) \
+    CRPCModEventData<type, CRPCModEventListener, body>
+
+typedef TYPE_RPCMOD_EVENT(EVENT_RPCMOD_UPDATE_NEW_BLOCK, CBlockEx) CRPCModEventUpdateNewBlock;
+typedef TYPE_RPCMOD_EVENT(EVENT_RPCMOD_UPDATE_NEW_TRANSACTION, CTransaction) CRPCModEventUpdateNewTx;
+
+class CRPCModEventListener : virtual public CEventListener
+{
+public:
+    virtual ~CRPCModEventListener() {}
+    DECLARE_EVENTHANDLER(CRPCModEventUpdateNewBlock);
+    DECLARE_EVENTHANDLER(CRPCModEventUpdateNewTx);
 };
 
 } // namespace bigbang
