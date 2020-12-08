9887297b8c5878aacdafb89c8279c5714ecc2c4f
MathxH Chen <brainfvck@foxmail.com>
2020-12-08 13:43:34 +0800

    Restore

--- src/bigbang/rpcmod.cpp
+++ src/bigbang/rpcmod.cpp
@@ -1603,15 +1603,15 @@ CRPCResultPtr CRPCMod::RPCGetBalance(CRPCParamPtr param)
 
     uint64 nNonce = 0;
 
-    for (int i = 0; i < 100000; ++i)
-    {
-        CRPCModEventUpdateNewBlock* pUpdateNewBlockEvent = new CRPCModEventUpdateNewBlock(nNonce, pCoreProtocol->GetGenesisBlockHash(), 0);
+    // for (int i = 0; i < 100000; ++i)
+    // {
+    //     CRPCModEventUpdateNewBlock* pUpdateNewBlockEvent = new CRPCModEventUpdateNewBlock(nNonce, pCoreProtocol->GetGenesisBlockHash(), 0);
 
-        CBlockEx block;
-        pCoreProtocol->GetGenesisBlock(block);
-        pUpdateNewBlockEvent->data = block;
-        pPusher->PostEvent(pUpdateNewBlockEvent);
-    }
+    //     CBlockEx block;
+    //     pCoreProtocol->GetGenesisBlock(block);
+    //     pUpdateNewBlockEvent->data = block;
+    //     pPusher->PostEvent(pUpdateNewBlockEvent);
+    // }
 
     //getbalance (-f="fork") (-a="address")
     uint256 hashFork;
