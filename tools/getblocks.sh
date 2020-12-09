ca1fc32934128a78caa266fbdff7db4386d55513
MathxH Chen <brainfvck@foxmail.com>
2020-12-09 10:36:36 +0800

    update

--- tools/getblocks.sh
+++ tools/getblocks.sh
@@ -2,7 +2,7 @@
  
 while :
 do
-    curl -d '{"id":69,"method":"getblocks","jsonrpc":"2.0","params":{"fork":"00000000b0a9be545f022309e148894d1e1c853ccac3ef04cb6f5e5c70f41a70","blockhashes":["awdawrawraw44242", "00000000b0a9be545f022309e148894d1e1c853ccac3ef04cb6f5e5c70f41a70"], "num":500}}' http://192.168.199.180:9906
+    curl -d '{"id":69,"method":"getblocks","jsonrpc":"2.0","params":{"fork":"00000000b0a9be545f022309e148894d1e1c853ccac3ef04cb6f5e5c70f41a70","blockhashes":["awdawrawraw44242", "00000000b0a9be545f022309e148894d1e1c853ccac3ef04cb6f5e5c70f41a70"], "num":500}}' http://127.0.0.1:9902
 done
  
  
