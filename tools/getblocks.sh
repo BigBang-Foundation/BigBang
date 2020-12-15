c9aca96f13471de313c4d0c39adea3d25c7e9382
MathxH Chen <brainfvck@foxmail.com>
2020-12-15 15:41:33 +0800

    Update testbash

--- tools/getblocks.sh
+++ tools/getblocks.sh
@@ -2,7 +2,7 @@
  
 while :
 do
-    curl -d '{"id":69,"method":"getblocks","jsonrpc":"2.0","params":{"fork":"0000000006854ebdc236f48dbbe5c87312ea0abd7398888374b5ee9a5eb1d291","blockhashes":["awdawrawraw44242", "0000000006854ebdc236f48dbbe5c87312ea0abd7398888374b5ee9a5eb1d291"], "num":500}}' http://127.0.0.1:9902
+    curl -d '{"id":69,"method":"getblocks","jsonrpc":"2.0","params":{"fork":"0000000006854ebdc236f48dbbe5c87312ea0abd7398888374b5ee9a5eb1d291","blockhashes":["awdawrawraw44242", "0000000006854ebdc236f48dbbe5c87312ea0abd7398888374b5ee9a5eb1d291"], "num":500}}' http://127.0.0.1:9908/sync/rpc
 done
  
  
